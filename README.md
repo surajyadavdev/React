85997a5cb5b6b7a19cfe219d50446a6f4e5fa25e
surajyadavdev <surajyadavrec55@gmail.com>
2023-10-31 17:53:37 +0530

    lets get hooked

---
+++ README.md
@@ -0,0 +1,23 @@
+#Namste React Learning
+
+#namaste Food
+/**
+ * Header
+ *  logo
+ *  navbar
+ * 
+ * Body
+ *  search
+ *  Restaurant Container
+ *   restaurant cards
+ *    name
+ *    image
+ *     start rating, cuisine,price for two
+ *     
+ * 
+ * 
+ * Footer
+ *  copyright
+ *  address
+ *  some link
+ */
---
+++ src/components/Body.js
@@ -0,0 +1,31 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/MockData";
+import { useState } from "react";
+const Body = ()=>{
+  const [listOfRestaurants,setListOfRestaurants] = useState(resList);
+    return(
+      <div className="body">
+        <div  className="filter">
+          <button 
+            className="filter-btn"
+            onClick={() =>{
+              const filteredList = listOfRestaurants.filter(
+                (res) =>res.data.avgRating >4
+                );
+                setListOfRestaurants(filteredList);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
+        <div className="res-container">
+         {
+          listOfRestaurants.map((restaurant)=>(
+            <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+  export default Body;
